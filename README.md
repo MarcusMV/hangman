d17d3719a2922863151d153d6b37f9a092a7ac53
Marcus Vo <MarcusMV@hotmail.co.uk>
2023-02-19 18:09:56 +0000

    prompt for input & update readme

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+
+## Milestone 1
+* The user is prompted to enter an input, this input is validated to be a single alphabetic character.
---
+++ milestone_2.py
@@ -0,0 +1,17 @@
+import random
+import re
+
+word_list = ['apples', 'guavas', 'bananas', 'pears', 'blueberries']
+print(word_list )
+
+word = random.choice(word_list)
+print(word)
+
+while True:
+    guess = input('Enter a single letter: ')
+    if len(guess) == 1 and re.match('[a-z]', guess.lower()):
+        print('Good guess!')
+        break
+    else:
+        print('Sorry, this is not a valid input.')
+        continue
