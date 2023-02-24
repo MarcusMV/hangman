9453a1b20b47fc13706f3f898e0ff1763374a03e
Marcus Vo <MarcusMV@hotmail.co.uk>
2023-02-24 16:24:02 +0000

    verify milestone_2 task_3

--- milestone_2.py
+++ milestone_2.py
@@ -7,11 +7,11 @@ word_list = ['apples', 'guavas', 'bananas', 'pears', 'blueberries']
 word = random.choice(word_list)
 print(word)
 
-# while True:
-#     guess = input('Enter a single letter: ')
-#     if len(guess) == 1 and re.match('[a-z]', guess.lower()):
-#         print('Good guess!')
-#         break
-#     else:
-#         print('Sorry, this is not a valid input.')
-#         continue
+while True:
+    guess = input('Enter a single letter: ')
+    if len(guess) == 1 and re.match('[a-z]', guess.lower()):
+        print('Good guess!')
+        break
+    else:
+        print('Sorry, this is not a valid input.')
+        continue
